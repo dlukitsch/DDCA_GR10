da6aefb65a5433b5fe11ff1d6593c253ca75c4be
STAstefan <38861896+STAstefan@users.noreply.github.com>
2018-05-03 13:25:00 +0200

    Add files via upload

--- EX3/src/regfile.vhd
+++ EX3/src/regfile.vhd
@@ -19,6 +19,31 @@ end regfile;
 
 architecture rtl of regfile is
 
+SUBTYPE word is STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);  
+TYPE reg_type is ARRAY(0 to (2**REG_BITS)-1) OF word; 
+signal registers : reg_type := (others=>(others=>'0'));
+
+signal int_rdaddr1, int_rdaddr2 : std_logic_vector(REG_BITS-1 downto 0) := (others => '0');
+
 begin  -- rtl
-	
+
+	process(clk)
+	begin
+		if reset = '0' then
+			int_rdaddr1 <= (others => '0');
+			int_rdaddr2 <= (others => '0');
+		elsif rising_edge(clk) and stall = '0' then
+			int_rdaddr1 <= rdaddr1;
+			int_rdaddr2 <= rdaddr2;
+
+			if rdaddr1 = wraddr or rdaddr2 = wraddr then
+				if regwrite = '1' and (to_integer(unsigned(wraddr)) /= 0) then
+					registers(to_integer(unsigned(wraddr))) <= wrdata;
+				end if;
+			end if;
+		end if;
+	end process;
+
+	rddata1 <= registers(to_integer(unsigned(int_rdaddr1)));
+	rddata2 <= registers(to_integer(unsigned(int_rdaddr2)));
 end rtl;
