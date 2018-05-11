f6420701abf00631128b04f05609f50816a8eade
STAstefan <38861896+STAstefan@users.noreply.github.com>
2018-05-11 19:01:47 +0200

    Add files via upload

--- EX3/src/regfile.vhd
+++ EX3/src/regfile.vhd
@@ -18,28 +18,32 @@ entity regfile is
 end regfile;
 
 architecture rtl of regfile is
-
-SUBTYPE word is STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);  
-TYPE reg_type is ARRAY(0 to (2**REG_BITS)-1) OF word; 
-signal registers : reg_type := (others=>(others=>'0'));
+ 
+TYPE reg_type is ARRAY(0 to (2**REG_BITS)-1) OF STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0); 
+signal registers : reg_type := (others => (others=> '0'));
 
 signal int_rdaddr1, int_rdaddr2 : std_logic_vector(REG_BITS-1 downto 0) := (others => '0');
+signal zero : std_logic_vector(REG_BITS-1 downto 0) := (others => '0');
 
 begin  -- rtl
 
-	process(clk)
+	sync : process(clk)
 	begin
 		if reset = '0' then
 			int_rdaddr1 <= (others => '0');
 			int_rdaddr2 <= (others => '0');
+
 		elsif rising_edge(clk) and stall = '0' then
 			int_rdaddr1 <= rdaddr1;
 			int_rdaddr2 <= rdaddr2;
+		end if;
+	end process;
 
-			if rdaddr1 = wraddr or rdaddr2 = wraddr then
-				if regwrite = '1' and (to_integer(unsigned(wraddr)) /= 0) then
-					registers(to_integer(unsigned(wraddr))) <= wrdata;
-				end if;
+	write : process(wraddr,int_rdaddr1,int_rdaddr2,regwrite)
+	begin
+		if int_rdaddr1 = wraddr or int_rdaddr2 = wraddr then
+			if regwrite = '1' and to_integer(unsigned(wraddr))/=0 then
+				registers(to_integer(unsigned(wraddr))) <= wrdata;
 			end if;
 		end if;
 	end process;
